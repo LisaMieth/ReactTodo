83fd7fea23c4519ff8976ade3aa18b1f12a353b3
LisaMieth <lisahafizovic@gmail.com>
2017-02-12 12:27:05 +1100

    Add root component

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var Main = require('Main');
+var TodoApp = require('TodoApp');
+
 
 //Load foundation
 $(document).foundation();
@@ -12,6 +13,6 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
     <Router history={hashHistory}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={TodoApp}>
    </Route>
 </Router>, document.getElementById('app'));
