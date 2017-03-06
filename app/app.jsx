cb9c84c2e4c2ea850069319fd79a5320c2033c1a
LisaMieth <lisahafizovic@gmail.com>
2017-03-06 19:30:34 +1100

    Refactor app to work with local storage

--- app/app.jsx
+++ app/app.jsx
@@ -4,19 +4,19 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var {Provider} = require('react-redux');
 
 var TodoApp = require('TodoApp');
-
+var TodoAPI = require('TodoAPI');
 var actions = require('actions');
 var store = require('configureStore').configure();
 
 store.subscribe(() => {
-  console.log('New state');
-  console.log(store.getState());
+  var state = store.getState();
+  console.log('New state', state);
+
+  TodoAPI.setTodos(state.todos);
 });
 
-store.dispatch(actions.addTodo('Clean the garden'));
-store.dispatch(actions.setSearchText('garden'));
-store.dispatch(actions.toggleShowCompleted());
-
+var initialTodos = TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 //Load foundation
 $(document).foundation();
 
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -23,6 +23,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate add todos action', () => {
+    var todos = [
+      {
+        id: '111',
+        text: 'Anything',
+        completed: false,
+        completedAt: null,
+        createdAt: 3300
+      }
+    ];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should generate toggle showCompleted action', () => {
     var action = {
       type: 'TOGGLE_SHOW_COMPLETED',
